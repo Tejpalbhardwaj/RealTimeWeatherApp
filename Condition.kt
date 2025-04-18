5b402dba03b06501808d31757a2e8f001a22f304
Tejpal Bhardwaj <104243802+Tejpalbhardwaj@users.noreply.github.com>
2025-04-18 11:34:55 +0530

    Add files via upload

---
+++ Condition.kt
@@ -0,0 +1,7 @@
+package com.example.realtimeweatherapp.Model
+
+data class Condition(
+    val code: String,
+    val icon: String,
+    val text: String
+)
---
+++ WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.example.realtimeweatherapp.Model
+
+data class WeatherModel(
+    val current: Current,
+    val location: Location
+)
